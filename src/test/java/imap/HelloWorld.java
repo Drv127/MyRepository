3cf0f10cfb8207effdecf5fccc6686760cd70b3f
Drv12 <st.ray.butenko@gmail.com>
2019-04-07 21:44:55 +0300

    Sps

---
+++ src/test/java/imap/HelloWorld.java
@@ -0,0 +1,19 @@
+package imap;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorld {
+ /*   public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+*/
+ public static void main (String []args) throws InterruptedException {
+     WebDriver driver= new ChromeDriver();
+     driver.get("https://google.com");
+     Thread.sleep(5000);
+     driver.quit();
+
+ }
+}
